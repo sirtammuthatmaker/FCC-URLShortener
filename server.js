5335152a792d29516240eb80b7c5699abb5125a5
hatmaker <riteshx82@gmail.com>
2020-07-11 22:26:01 -0700

    api endpoints setup

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+const dns = require('dns');
 require('dotenv').config();
 
 var cors = require('cors');
@@ -23,8 +25,8 @@ mongoose.connect(process.env.DB_URI,{ useNewUrlParser: true, useUnifiedTopology:
 
 app.use(cors());
 
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+
+app.use(bodyParser.json(),bodyParser.urlencoded({extended:false}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -38,9 +40,25 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/shorturl/new", function (req, res){
-  res.json({"original_url":"www.google.com","short_url":1});
+app.get("/api/shorturl/:id", function (req, res) {
+  const idRecieved = req.params.id;
+  //handle shorturl ids
+});
+
+app.post("/api/shorturl/new", function (req, res){
+  
+  dns.lookup(req.body.url,(err)=>{
+    if(err) {
+    console.log("URL not valid!");
+  } else {
+    console.log("Sent by postman: "+req.body.url);
+    //handle valid urls
+  }
+  })
+  res.json({"original_url":req.body.url,"short_url":1});
 })
+
+
 
 app.listen(port, function () {
   console.log('Node.js listening: '+port);
